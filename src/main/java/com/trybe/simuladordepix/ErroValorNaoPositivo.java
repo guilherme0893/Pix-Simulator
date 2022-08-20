97f3cfb9fad16fbcaddf9470784357e9d4d27f41
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-08-19 22:58:46 -0300

    chore add error

---
+++ src/main/java/com/trybe/simuladordepix/ErroValorNaoPositivo.java
@@ -0,0 +1,11 @@
+package com.trybe.simuladordepix;
+
+/**
+ * Classe: ErroValorNaoPositivo.
+ */
+
+public class ErroValorNaoPositivo extends ErroDePix {
+  public ErroValorNaoPositivo() {
+    super(Mensagens.VALOR_NAO_POSITIVO);
+  }
+}
