25cff67df603e5f848eda701da74857a9cf810f9
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-08-19 22:59:32 -0300

    chore: add simulador de pix

--- src/main/java/com/trybe/simuladordepix/ControladorDePix.java
+++ src/main/java/com/trybe/simuladordepix/ControladorDePix.java
@@ -2,6 +2,10 @@ package com.trybe.simuladordepix;
 
 import java.io.IOException;
 
+/**
+ * Class Controldor.
+ */
+
 public class ControladorDePix {
 
   private final ProcessadorDePix processadorDePix;
@@ -16,11 +20,18 @@ public class ControladorDePix {
    *
    * @param valor Valor em centavos a ser transferido.
    * @param chave Chave Pix do beneficiário da transação.
-   *
    * @return Mensagem a ser exibida à pessoa usuária, informando-a sobre
-   *         o resultado da operação.
    */
   public String aoConfirmarPix(int valor, String chave) {
-    return null; // TODO: Implementar.
+
+    try {
+      processadorDePix.executarPix(valor, chave);
+
+      return Mensagens.SUCESSO;
+    } catch (ErroDePix error) {
+      return error.getMessage();
+    } catch (IOException e) {
+      return Mensagens.ERRO_DE_CONEXAO;
+    }
   }
 }
